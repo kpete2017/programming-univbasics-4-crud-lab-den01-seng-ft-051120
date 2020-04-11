bda8e45e1efa29c2431a5a70ab753e7c3c194f00
Kyle Petersen <kpete2017@gmail.com>
2020-04-11 01:58:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve = array[index_number]
-  return retrieve
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
